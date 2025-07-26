1bd9d59caa332021f2918baf9bc7e2f6a6ba4fb4
“Alexis <alexis.t.hampton@gmail.com>
2025-07-26 15:36:09 -0400

    added LinkedIn link

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -6,9 +6,10 @@ const Navbar = () => {
         <div className='bg-base-300  mx-auto p-4  flex justify-between'>
             <Link to="/" className='card-title text-3xl '> Alexis Hampton</Link>
             <div>
-                <a className='link link-neutral mx-2' href="https://github.com/AlexisHampton" target='blank'> Github </a>
-                <a className='link link-neutral mx-2' href="https://github.com/xXClawtheWolfXx" target='blank'> Personal Github</a>
-                <a className='link link-neutral mx-2' href="https://severedstars.itch.io/" target='blank'> Game Portfolio</a>
+                <a className='link link-neutral mx-2' href="https://github.com/AlexisHampton" target='blank'>Github</a>
+                <a className='link link-neutral mx-2' href="https://github.com/xXClawtheWolfXx" target='blank'>Personal Github</a>
+                <a className='link link-neutral mx-2' href="https://severedstars.itch.io/" target='blank'>Game Portfolio</a>
+                <a className='link link-neutral mx-2' href="https://www.linkedin.com/in/alexis-hampton/" target='blank'>LinkedIn</a>
             </div>
         </div>
     )
