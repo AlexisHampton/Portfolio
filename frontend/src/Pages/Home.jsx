ffa6a88506eb87ff7ea9d54a9fee6cf8f29e4403
“Alexis <alexis.t.hampton@gmail.com>
2025-07-25 22:09:04 -0400

    added firebase hosting

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
                         <h1 className='font-bold text-7xl text-center'> Alexis Hampton</h1>
 
                         <h4 className='text-center text-3xl'> Aspiring Software Developer</h4>
-                        <h3 className='text-lg'> Detail-oriented Computer Science graduate with a strong interest in backend development and building scalable, maintainable solutions that keep customers first. Experienced with the MERN and .NET stacks and adept at designing RESTful APIs. Eager to apply my technical expertise in a collaborative environment that values continuous learning and innovation. </h3>
+                        <h3 className='text-lg'>  Computer Science graduate with a strong interest in backend development and building scalable, maintainable solutions that keep customers first. Experienced with the MERN and .NET stacks and adept at designing RESTful APIs. Eager to apply my technical expertise in a collaborative environment that values continuous learning and innovation. </h3>
                     </div>
                 </div>
             </div>
