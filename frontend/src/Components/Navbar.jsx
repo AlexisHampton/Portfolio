c14aeed01c02d8d39e97414756bcc03c83774c80
“Alexis <alexis.t.hampton@gmail.com>
2025-07-26 15:34:29 -0400

    Added important links

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router'
 
 const Navbar = () => {
     return (
-        <div className='bg-base-300 mx-auto p-4 w-screen'>
+        <div className='bg-base-300  mx-auto p-4  flex justify-between'>
+            <Link to="/" className='card-title text-3xl '> Alexis Hampton</Link>
             <div>
-                <Link to="/" className='card-title text-3xl '> Alexis Hampton</Link>
-            </div>
-            <div>
-                <h4></h4>
+                <a className='link link-neutral mx-2' href="https://github.com/AlexisHampton" target='blank'> Github </a>
+                <a className='link link-neutral mx-2' href="https://github.com/xXClawtheWolfXx" target='blank'> Personal Github</a>
+                <a className='link link-neutral mx-2' href="https://severedstars.itch.io/" target='blank'> Game Portfolio</a>
             </div>
         </div>
     )
